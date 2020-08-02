3d47b5c195320d885559f9a6164f873d920324c1
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-02 17:39:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,14 @@
 class Anagram
-  attr_accessor :diaper
-  
-  def initialize (diaper)
-    @diaper = diaper
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
   end
 
-
-end   #Class end
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end   #class end
